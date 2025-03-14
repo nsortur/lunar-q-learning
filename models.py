76a1c6b1a504a5c42cb226d733fe7151ef657b08
Neel Sortur <sortur.n@northeastern.edu>
2025-03-14 16:26:27 -0400

    Add scaffold for q learning

--- models.py
+++ models.py
@@ -2,3 +2,15 @@ import torch
 from torch import nn
 
 # state -> q values for each action
+class DeepQ(nn.Module):
+    def __init__(self, in_dim=8, hidden_neurons=128, num_actions=4):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(in_dim, hidden_neurons),
+            nn.ReLU(),
+            nn.Linear(hidden_neurons, num_actions),
+        )
+        
+    def forward(self, state):
+        x = self.network(state) # 8 -> hidden_neurons -> 4
+        return x # [0.2, 0.4, ...]
