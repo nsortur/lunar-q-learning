f1c3e51cd12a01be379e61fe843e4e2c119501bd
Neel Sortur <sortur.n@northeastern.edu>
2025-03-21 14:26:09 -0400

    Basic q learning and new packages

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -1,52 +1,70 @@
 import random
 import torch
+from torch.optim import SGD
 import gymnasium as gym
 import numpy as np
-from collections import defaultdict
+from models import DeepQ
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 env = gym.make("LunarLander-v3", continuous=False, gravity=-10.0,
-               enable_wind=False, wind_power=15.0, turbulence_power=1.5, render_mode="human")
+               enable_wind=False, wind_power=15.0, turbulence_power=1.5)
 
+# device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+device = 'cpu'
 
-def Q_learning(num_episodes=10000, gamma=0.9, epsilon=1, alpha=0.9, decay_rate=0.999):
+def Q_learning(num_episodes=10000, gamma=0.9, epsilon=1, alpha=0.01, decay_rate=0.999):
 
-    Q_table = defaultdict()
+    model = DeepQ(in_dim=8, num_actions=4).to(device)
+    optim = SGD(model.parameters(), lr=alpha)
 
     done = False
-    for _ in range(num_episodes):
+    rewards = []
+    for _ in tqdm(range(num_episodes)):
+        done = False
         obs, info = env.reset()
-
+        episode_reward = []
         while not done:
-            state = hash(str(obs))
+            
+            q_vals = model(torch.tensor(obs, device=device))
 
             if random.random() < epsilon:
                 # random legal action
                 action = env.action_space.sample()
             else:
                 # optimal action
-                action = np.argmax(Q_table[state])
+                action = torch.argmax(q_vals).cpu().item()
             
-
             obs, reward, done, truncated, info = env.step(action)
-
+            # print(reward)
+            episode_reward.append(reward)
+            
             # update q-table
-            new_state = hash(str(obs))
-            q_val = Q_table[state][action]
+            q_val = q_vals[action]
             
             # action = env.action_space.sample()
             # env.render()
             # time.sleep(0.1)
             
-            # replace with q learning update rule
-            # https://huggingface.co/learn/deep-rl-course/en/unit3/deep-q-algorithm
-            Q_table[state][action] = q_val + (alpha * (reward + (gamma * np.max(Q_table[new_state])) - q_val))
+            q_val_next = model(torch.tensor(obs))
+            q_target = (reward + (gamma * torch.max(q_val_next)) - q_val)
+            
+            loss = q_target - q_val
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
 
         epsilon *= decay_rate
+        rewards.append(np.sum(episode_reward))
 
-    return Q_table
+    print(epsilon)
+    return rewards
 
 
-decay_rate = 0.99999
+decay_rate = 0.99
 
 # default: Q_table = Q_learning(num_episodes=1000000, gamma=0.9, epsilon=1, decay_rate=decay_rate)
-Q_table = Q_learning(num_episodes=10, gamma=0.9, epsilon=1, alpha=0.9, decay_rate=decay_rate)  # Run Q-learning
+rewards = Q_table = Q_learning(num_episodes=2000, gamma=0.9, epsilon=1, alpha=0.01, decay_rate=decay_rate)  # Run Q-learning
+plt.plot(rewards)
+plt.show()
+
